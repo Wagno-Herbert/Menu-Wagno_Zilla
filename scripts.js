60e890c30a09777665d4f8e5962bf33b1ca32ccc
wagno Herbert <betobahiano@gmail.com>
2024-09-30 14:52:54 -0300

    commint final

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
-const botao = document.querySelector('botao-menu')
+const botao =  document.querySelector('.botao-menu')
 const menuLateral = document.querySelector('.menu-lateral')
-const conteudo  = document.querySelector('.conteudo')
-const background =  document.querySelector('.background')
+const conteudo =  document.querySelector('.conteudo')
+const background = document.querySelector('.background')
 
 botao.addEventListener('click', () => {
-    menuLateral.classList.add('ativo')
-    conteudo.classList.add('ativo')
-    background.classList.add('ativo')
-    botao.classList.add('ativo')
-    document.body.style.background = menuLateral.classList.contains('ativo') ? '#34495e':'#ecf0f1'
-    
+    menuLateral.classList.toggle('ativo')
+    background.classList.toggle('ativo')
+    botao.classList.toggle('ativo')
+    conteudo.classList.toggle('ativo')
+    document.body.style.backgroundColor =  menuLateral.classList.contains('ativo') ? '#34495e' : '#ecf0f1'
+
 })
+background.addEventListener('click', () => {
+    menuLateral.classList.remove('ativo')
+    background.classList.remove('ativo')
+    botao.classList.remove('ativo')
+    conteudo.classList.remove('ativo')
+    document.body.style.backgroundColor = "#ecf0f1"
+
+})
+    
